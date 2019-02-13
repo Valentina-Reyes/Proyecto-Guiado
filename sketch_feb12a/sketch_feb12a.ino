8fc213e68432b1d3cad4f52aac7b9efdfd8d1bc2
Nataliasporras <47115649+Nataliasporras@users.noreply.github.com>
2019-02-12 22:39:48 -0500

    Sensor de movimiento

---
+++ sketch_feb12a/sketch_feb12a.ino
@@ -0,0 +1,1 @@
+int sensorPin =5;int ledPin = 2;void setup() {    pinMode(sensorPin, INPUT);    pinMode(ledPin, OUTPUT);}void loop() {    if (digitalRead(sensorPin)== HIGH){      digitalWrite(ledPin, HIGH);    } else {      digitalWrite(ledPin, LOW);    }}
