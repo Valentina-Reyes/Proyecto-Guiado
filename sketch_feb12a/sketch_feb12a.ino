de5071b346cfe6ccbfe4823eec907e068427f272
Nataliasporras <47115649+Nataliasporras@users.noreply.github.com>
2019-02-17 08:57:17 -0500

    Update sketch_feb12a.ino

--- sketch_feb12a/sketch_feb12a.ino
+++ sketch_feb12a/sketch_feb12a.ino
@@ -1,1 +1,18 @@
-int sensorPin =5;int ledPin = 2;void setup() {    pinMode(sensorPin, INPUT);    pinMode(ledPin, OUTPUT);}void loop() {    if (digitalRead(sensorPin)== HIGH){      digitalWrite(ledPin, HIGH);    } else {      digitalWrite(ledPin, LOW);    }}
+int sensorPin =5;
+
+int ledPin = 2;
+
+
+
+void setup() {
+    pinMode(sensorPin, INPUT);
+    pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+    if (digitalRead(sensorPin)== HIGH){
+      digitalWrite(ledPin, HIGH);
+    } else {
+      digitalWrite(ledPin, LOW);
+    }
+}
